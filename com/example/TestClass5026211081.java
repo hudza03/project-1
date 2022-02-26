3cfbc8b4f30496649e39256e73b358f793c00e28
hudza03 <100013941+hudza03@users.noreply.github.com>
2022-02-26 16:57:06 +0700

    Update TestClass5026211081.java

--- com/example/TestClass5026211081.java
+++ com/example/TestClass5026211081.java
@@ -1,9 +1,7 @@
 
-package com.example;
-
 public class TestClass5026211081 {
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         ComputeMethods5026211081 call = new ComputeMethods5026211081();
         call.fToC(70);
         call.hypotenuse(3, 4);
