249a552cef43432054c25c3b0be325dfe95b1a47
hudza03 <100013941+hudza03@users.noreply.github.com>
2022-02-26 21:30:13 +0700

    Update ProcessAName5026211081.java

--- ProcessAName5026211081.java
+++ ProcessAName5026211081.java
@@ -1,23 +1,19 @@
 import java.util.Scanner;
 
-
 public class ProcessAName5026211081 {
     
-    
-public static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         System.out.print("Enter your full name: ");
         String name = scn.nextLine();
         int spaceIdx = name.indexOf(' '); 
         String firstName = name.substring(0,1).toUpperCase();
         
-        
         spaceIdx++;
         String lastIn = name.substring(spaceIdx, spaceIdx+1).toUpperCase();
         spaceIdx++;
         String lastName = name.substring(spaceIdx);
         
-        
-        System.out.println("Your name is : " +lastIn+lastName+ ", " +firstName+ ".");
+                System.out.println("Your name is : " +lastIn+lastName+ ", " +firstName+ ".");
     }
 }
